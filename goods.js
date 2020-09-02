ac12a4918a12ef4531bd59b0f61b595aa1fa4c38
Roman <roman82direct@yandex.ru>
2020-09-02 16:08:32 +0300

    сделан массив товаров

---
+++ goods.js
@@ -0,0 +1,40 @@
+var goods = [
+    {
+    title: 'assassin’s creed: rogue',
+    shortDiscr: 'The plot is set in a fictional history of real world events and follows the centuries-old',
+    longDiscr: 'The plot is set in a fictional history of real world events and follows the centuries-old struggle between the Assassins, who fight for peace with free will, and the Templars, who desire peace through control. The story is set in the mid-18th century during the Seven Years\' War, and follows Shay Patrick Cormac, an Assassin-turned-Templar who hunts down former members of his Brotherhood after being betrayed by them. Gameplay in Rogue is very similar to that of Black Flag with a mixture of ship-based naval exploration and third-person land-based exploration with some new features.',
+    price: 100,
+    quant: 10,
+    photoForGallery: ['img/assassins/forgallery/ACreed_0.jpg', 'img/assassins/forgallery/ACreed_3.jpg', 'img/assassins/forgallery/ACreed_4.jpg'],
+    bigPhoto: ['img/assassins/big/ACreed_1.jpg', 'img/assassins/big/ACreed_2.jpg', 'img/assassins/big/ACreed_3.jpg', 'img/assassins/big/ACreed_4.jpg'],
+    smallPhoto: ['img/assassins/small/ACreed_1.jpg', 'img/assassins/small/ACreed_2.jpg', 'img/assassins/small/ACreed_3.jpg', 'img/assassins/small/ACreed_4.jpg'],
+    order: false,
+    buy: false
+    },
+    {
+    title: 'world of tanks',
+    shortDiscr: 'The plot is set in a fictional history of real world events and follows the centuries-old',
+    longDiscr: 'The plot is set in a fictional history of real world events and follows the centuries-old struggle between the Assassins, who fight for peace with free will, and the Templars, who desire peace through control. The story is set in the mid-18th century during the Seven Years\' War, and follows Shay Patrick Cormac, an Assassin-turned-Templar who hunts down former members of his Brotherhood after being betrayed by them. Gameplay in Rogue is very similar to that of Black Flag with a mixture of ship-based naval exploration and third-person land-based exploration with some new features.',
+    price: 90,
+    quant: 20,
+    photoForGallery: ['img/woftanks/forgallery/tanks_1.jpg', 'img/woftanks/forgallery/tanks_2.jpg', 'img/woftanks/forgallery/tanks_3.jpg', 'img/woftanks/forgallery/tanks_4.jpeg'],
+    bigPhoto: ['img/woftanks/big/tanks_1.jpg', 'img/woftanks/big/tanks_2.jpg', 'img/woftanks/big/tanks_3.jpg', 'img/woftanks/big/tanks_4.jpeg'],
+    smallPhoto: ['img/woftanks/small/tanks_1.jpg', 'img/woftanks/small/tanks_2.jpg', 'img/woftanks/small/tanks_3.jpg', 'img/woftanks/small/tanks_4.jpeg'],
+    order: false,
+    buy: false
+    },
+    {
+    title: 'world of warships',
+    shortDiscr: 'The plot is set in a fictional history of real world events and follows the centuries-old',
+    longDiscr: 'The plot is set in a fictional history of real world events and follows the centuries-old struggle between the Assassins, who fight for peace with free will, and the Templars, who desire peace through control. The story is set in the mid-18th century during the Seven Years\' War, and follows Shay Patrick Cormac, an Assassin-turned-Templar who hunts down former members of his Brotherhood after being betrayed by them. Gameplay in Rogue is very similar to that of Black Flag with a mixture of ship-based naval exploration and third-person land-based exploration with some new features.',
+    price: 85,
+    quant: 15,
+    photoForGallery: ['img/wofws/forgallery/Ships_0.png', 'img/wofws/forgallery/Ships_1.jpg', 'img/wofws/forgallery/Ships_2.jpg', 'img/wofws/forgallery/Ships_3.jpg', 'img/wofws/forgallery/Ships_4.jpg'],
+    bigPhoto: ['img/wofws/big/Ships_1.jpg', 'img/wofws/big/Ships_2.jpg', 'img/wofws/big/Ships_3.jpg', 'img/wofws/big/Ships_3.jpg'],
+    smallPhoto: ['img/wofws/small/Ships_1.jpg', 'img/wofws/small/Ships_2.jpg', 'img/wofws/small/Ships_3.jpg', 'img/wofws/small/Ships_4.jpg'],
+    order: false,
+    buy: false
+    },
+];
+
+var order = [];
